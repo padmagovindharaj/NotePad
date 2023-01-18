4f16a2f3e1512955dc3ed22f2deb4517565d777f
padmagovindharaj <122960323+padmagovindharaj@users.noreply.github.com>
2023-01-18 12:10:30 +0530

    Add files via upload
    Initial commit

---
+++ Notepad-HandsOn/src/main/java/com/notepad/NotepadHandsOn/NotepadHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.notepad.NotepadHandsOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadHandsOnApplication.class, args);
+	}
+
+}
---
+++ Notepad-HandsOn/src/test/java/com/notepad/NotepadHandsOn/NotepadHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notepad.NotepadHandsOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
